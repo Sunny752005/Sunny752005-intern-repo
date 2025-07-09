649e0b6d785126561d29653083ae41edead13af5
Arman Gollen <armangollen75@gmail.com>
2025-07-09 15:24:12 +1000

    restructuring repo

---
+++ react-code/src/App.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import TestApi from './TestApi';
+
+function App() {
+  return (
+    <div style={{ padding: '2rem', fontFamily: 'sans-serif' }}>
+      <h1>Axios API Test</h1>
+      <TestApi />
+    </div>
+  );
+}
+
+export default App;
