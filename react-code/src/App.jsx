b7e2c9da7bdaaaf35c696ef54921459dd77f6359
Arman Gollen <armangollen75@gmail.com>
2025-07-18 14:09:56 +1000

    code for Handling Forms with Formik

--- react-code/src/App.jsx
+++ react-code/src/App.jsx
@@ -1,11 +1,11 @@
 import React from 'react';
-import Greeting from './Implementing-Internationalization-with-i18next/Greeting';
+import MyForm from './Handling-Forms-with-Formik/MyForm';
 
 function App() {
   return (
     <div style={{ padding: '2rem', fontFamily: 'sans-serif' }}>
-      <h1>i18next Test</h1>
-      <Greeting />
+      <h1>Formik Form</h1>
+      <MyForm />
     </div>
   );
 }
