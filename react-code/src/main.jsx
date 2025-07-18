9c300cdcb3d15e068d27e8c761c5d706fbea4587
Arman Gollen <armangollen75@gmail.com>
2025-07-18 13:24:59 +1000

    updated the file structure

--- react-code/src/main.jsx
+++ react-code/src/main.jsx
@@ -1,11 +1,11 @@
 import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
 import './index.css';
-import './Implementing-Internationalization-in-React/i18n/i18n'; // ✅ Correct path
+import './Implementing-Internationalization-in-React/i18n/i18n'; // Correct path to i18n.js
 import App from './App.jsx';
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
     <App />
-  </StrictMode>,
+  </StrictMode>
 );
