2d6e184bd7bea0214ef12d361e42f1f6de940082
Arman Gollen <armangollen75@gmail.com>
2025-07-18 13:36:15 +1000

    fixed navigation issue

--- react-code/src/main.jsx
+++ react-code/src/main.jsx
@@ -1,7 +1,7 @@
 import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
 import './index.css';
-import "./Implementing-Internationalization-in-React/i18n/i18n";
+import './Implementing-Internationalization-with-i18next/i18n/i18n.js';
 import App from './App.jsx';
 
 createRoot(document.getElementById('root')).render(
