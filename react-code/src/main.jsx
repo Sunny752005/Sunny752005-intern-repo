b3af09199e24301530a518c5bfd291ae4a383013
Arman Gollen <armangollen75@gmail.com>
2025-07-23 15:23:21 +1000

    code changed for import locations

--- react-code/src/main.jsx
+++ react-code/src/main.jsx
@@ -2,10 +2,14 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import './Implementing-Internationalization-with-i18next/i18n/i18n.js';
-import './Using-Selectors-in-Redux-Toolkit/main.jsx';
+import App from './Using-Selectors-in-Redux-Toolkit/App.jsx';
+import store from './Using-Selectors-in-Redux-Toolkit/store.js';
+import { Provider } from 'react-redux';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <App />
+    <Provider store={store}>
+      <App />
+    </Provider>
   </React.StrictMode>
 );
