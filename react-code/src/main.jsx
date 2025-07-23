95239bf66e5cbb1d8dcd5a582e0106fd87ab8d61
Arman Gollen <armangollen75@gmail.com>
2025-07-23 15:48:50 +1000

    Introduction to Redux Toolkit code done

--- react-code/src/main.jsx
+++ react-code/src/main.jsx
@@ -1,9 +1,8 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
-import './Implementing-Internationalization-with-i18next/i18n/i18n.js';
-import App from './Using-Selectors-in-Redux-Toolkit/App.jsx';
-import store from './Using-Selectors-in-Redux-Toolkit/store.js';
+import App from './Introduction-to-Redux-Toolkit/App';
+import store from './Introduction-to-Redux-Toolkit/store';
 import { Provider } from 'react-redux';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
