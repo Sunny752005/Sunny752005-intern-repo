1857b3b90a4c39248f460027151def70121e3112
Arman Gollen <armangollen75@gmail.com>
2025-07-25 12:50:41 +1000

    changed locattion ref for this task

--- react-code/src/main.jsx
+++ react-code/src/main.jsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
-import App from './Working-with-Lists-User-Input/App';
+import App from './Handling-State-User-Input/App';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
